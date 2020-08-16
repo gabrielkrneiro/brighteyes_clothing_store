1654e8e497375208b1ff5e10661edbe5cb68039c
Gabriel Carneiro <gabrielcarneirocef@gmail.com>
2020-08-16 19:45:13 -0300

    Add Enums to represent Clothes, Employee, Client and ShoppingCart status

---
+++ backend/src/modules/clothes_status/ClothesStatusEnum.ts
@@ -0,0 +1,4 @@
+export enum ClothesStatusEnum {
+  IN_STOCK = 'IN STOCK',
+  OUT_OF_STOCK = 'OUT OF STOCK',
+}
---
+++ backend/src/modules/employee_title/EmployeeTitleEnum.ts
@@ -0,0 +1,7 @@
+export enum EmployeeTitleEnum {
+  WAREHOUSE = 'Warehouse',
+  SELLER = 'Seller',
+  CASHIER = 'Cashier',
+  CUSTOMER_SERVICE = 'Customer Service',
+  HUMAN_RESOURCE = 'Human Resource',
+}
