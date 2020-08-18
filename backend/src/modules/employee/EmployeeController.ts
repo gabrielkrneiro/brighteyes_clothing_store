459e15fc81b87faba3b908b00ecba96fd277acc8
Gabriel Carneiro <gabrielcarneirocef@gmail.com>
2020-08-18 05:35:30 -0300

    Add EmployeeFactory

--- backend/src/modules/employee/EmployeeController.ts
+++ backend/src/modules/employee/EmployeeController.ts
@@ -1,12 +1,16 @@
-import { Router } from 'express'
+import { getRepository } from 'typeorm'
+import { Request, Response, Router } from 'express'
 
 import { DTOController } from '@src/common/dto/DTOController'
 import { IController } from '@src/interfaces/IControllers'
 import { AbstractController } from '@src/modules/abstract.controller'
 import { Employee } from '@src/modules/employee/Employee'
+import { EmployeeCreateDTO } from './EmployeeDTO'
+import { EmployeeFactory } from './EmployeeFactory'
 
 export class EmployeeController extends AbstractController implements IController {
   route: Router
+  factory: EmployeeFactory
 
   constructor({ route }: DTOController) {
     super()
@@ -14,6 +18,7 @@ export class EmployeeController extends AbstractController implements IControlle
     this.route = route
     this.findManyOptions = { relations: ['title', 'status'] }
     this.findOneOptions = { relations: ['title', 'status'] }
+    this.factory = new EmployeeFactory()
   }
 
   async init(): Promise<void> {
@@ -22,5 +27,32 @@ export class EmployeeController extends AbstractController implements IControlle
     this.route.get('/employees/:id', this.listOneById)
     this.route.put('/employees/:id', this.update)
     this.route.delete('/employees/:id', this.remove)
+  }
+
+  create = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const data = request.body as EmployeeCreateDTO
+      const repository = getRepository(Employee)
+      const employeeData = await this.factory.buildWithCreateDTO(data)
+      const foundEmployee = await repository.findOne({ where: { email: data.email } })
+      if (foundEmployee) {
+        throw new Error('Employee already exists')
+      }
+      const createdObject = await repository.save(employeeData, this.saveOptions)
+      return response.json({
+        message: 'Employee created',
+        data: {
+          name: createdObject.name,
+          email: createdObject.email,
+          photo: createdObject.photo,
+        },
+      })
+    } catch (error) {
+      console.error(error)
+      return response.status(401).json({
+        message: 'An error occurred',
+        error_message: error.message,
+      })
+    }
   }
 }
---
+++ backend/src/modules/employee/EmployeeDTO.ts
@@ -0,0 +1,9 @@
+export interface EmployeeCreateDTO {
+  name: string
+  title: string
+  photo: string
+  birthdate: string
+  password: string
+  email: string
+  registeredBy: number
+}
