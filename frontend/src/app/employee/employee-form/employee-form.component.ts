3535259304b676b6a2db13e4ed0dfc3e495e3a5d
Gabriel Carneiro <gabrielcarneirocef@gmail.com>
2020-10-04 04:57:55 -0300

    Add form validation in employee module

--- frontend/src/app/employee/employee-form/employee-form.component.ts
+++ frontend/src/app/employee/employee-form/employee-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
@@ -40,7 +48,7 @@ const EmployeeCreateDtoDefaultValues = {
   styleUrls: ['./employee-form.component.scss'],
 })
 export class EmployeeFormComponent implements OnInit {
-  employeeForm: FormGroup;
+  formGroup: FormGroup;
   isUpdating: boolean;
 
   @Input() employeeStatusList: Observable<EmployeeStatus[]>;
@@ -53,7 +61,8 @@ export class EmployeeFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.employeeForm = this.fb.group({
+    this.isUpdating = false;
+    this.formGroup = this.fb.group({
       id: [null],
       name: [null, [Validators.required]],
       email: [null, [Validators.email, Validators.required]],
@@ -64,13 +73,28 @@ export class EmployeeFormComponent implements OnInit {
       title: [null, [Validators.required]],
       registeredBy: [null, [Validators.required]],
     });
-    this.isUpdating = false;
-    // this.employeeForm.patchValue(employeeMock); // fill form with mocked values
+
+    this.formGroup.get('id').valueChanges.subscribe((value) => {
+      if (value) {
+        this.formGroup.get('password').clearValidators();
+      } else {
+        this.formGroup.get('password').setValidators(Validators.required);
+      }
+      this.formGroup.get('password').updateValueAndValidity();
+    });
+  }
+
+  isValid(attr: string): string {
+    return this.formGroup.get(attr).valid ? 'is-valid' : 'is-invalid';
+  }
+
+  showInvalidFeedback(attr: string): boolean {
+    return this.isValid(attr) === 'is-invalid' ? true : false;
   }
 
   sendEmployee(): void {
-    const form = this.employeeForm.value;
-    form.birthdate = dateFormatter(this.employeeForm.controls.birthdate.value);
+    const form = this.formGroup.value;
+    form.birthdate = dateFormatter(this.formGroup.controls.birthdate.value);
     // console.log(form);
     try {
       if (!this.isUpdating) {
@@ -85,14 +109,26 @@ export class EmployeeFormComponent implements OnInit {
 
   setEmployeeToUpdate(employee: Employee): void {
     this.isUpdating = true;
-    this.employeeForm.patchValue(employee);
-    this.employeeForm.patchValue({ title: employee.title.id });
-    this.employeeForm.patchValue({ status: employee.status.id });
-    this.employeeForm.patchValue({ registeredBy: employee.registeredBy?.id });
+    this.formGroup.patchValue(employee);
+    this.formGroup.patchValue({ title: employee.title.id });
+    this.formGroup.patchValue({ status: employee.status.id });
+    this.formGroup.patchValue({ registeredBy: employee.registeredBy?.id });
   }
 
   resetForm(): void {
-    this.employeeForm.reset();
+    this.formGroup.reset();
     this.isUpdating = false;
+  }
+
+  teste(): void {
+    console.log(this.formGroup.get('id').errors);
+    console.log(this.formGroup.get('name').errors);
+    console.log(this.formGroup.get('email').errors);
+    console.log(this.formGroup.get('status').errors);
+    console.log(this.formGroup.get('photo').errors);
+    console.log(this.formGroup.get('birthdate').errors);
+    console.log(this.formGroup.get('password').errors);
+    console.log(this.formGroup.get('title').errors);
+    console.log(this.formGroup.get('registeredBy').errors);
   }
 }
